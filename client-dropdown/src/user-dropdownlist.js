e2ec70626a8a621a71ba2c643d0243787da5fde5
komalshivani <komalshivani2@gmail.com>
2019-07-13 23:44:09 -0400

    added react-strap card

--- client-dropdown/src/user-dropdownlist.js
+++ client-dropdown/src/user-dropdownlist.js
@@ -2,47 +2,47 @@ import React from 'react'
 import axios from 'axios'
 import UserdropdownShow from './user-dropdownshow';
 
-class UserdropdownList extends React.Component{
-    constructor(){
+class UserdropdownList extends React.Component {
+    constructor() {
         super()
-        this.state={
-            users:[],
-            user:{}
+        this.state = {
+            users: [],
+            user: {}
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         axios.get(`http://jsonplaceholder.typicode.com/users`)
-        .then(response=>{
-            console.log(response.data)
-            this.setState(()=>({
-                users:response.data
-            }))
-        })
+            .then(response => {
+                console.log(response.data)
+                this.setState(() => ({
+                    users: response.data
+                }))
+            })
     }
 
-    handleChange=(e)=>{
-        const user=e.target.value
+    handleChange = (e) => {
+        const user = e.target.value
         console.log(user)
-        this.setState(()=>({
-            user:user
+        this.setState(() => ({
+            user: user
         }))
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                
-                <form>
-                <select value={this.state.user} onChange={this.handleChange}>
-                    <option value="">select</option>
-                    {this.state.users.map(user=>{
-                        return <option key={user.id} value={user.id}>
-                            {user.name}
-                        </option>
-                    })}
 
-                </select>
+                <form>
+                    <select value={this.state.user} onChange={this.handleChange}>
+                        <option value="">select</option>
+                        {this.state.users.map(user => {
+                            return <option key={user.id} value={user.id}>
+                                {user.name}
+                            </option>
+                        })}
+
+                    </select>
                 </form>
-            {Object.keys(this.state.user).length>0 && <UserdropdownShow user={this.state.user}/>}
+                {Object.keys(this.state.user).length > 0 && <UserdropdownShow user={this.state.user} />}
             </div>
         )
     }
